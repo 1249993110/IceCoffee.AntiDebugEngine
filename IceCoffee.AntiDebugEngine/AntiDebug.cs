120036c9c5d7a81aa0533b162fc711224bfa549a
1249993110 <1249993110@qq.com>
2020-07-25 16:31:58 +0800

    create project

---
+++ IceCoffee.AntiDebugEngine/AntiDebug.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace IceCoffee.AntiDebugEngine
+{
+    public static class AntiDebug
+    {
+        public const string DllName = "AntiDebug_C.dll";
+
+        public const string DllMD5 = "13b0fde74cd2fd02ba7abbef33bd158d";
+
+        [DllImport(DllName, EntryPoint = "AntiDebug")]
+        private extern static bool AntiDebug_C();
+
+        /// <summary>
+        /// 返回true说明处于被调试状态
+        /// </summary>
+        /// <returns></returns>
+        public static bool AntiDebug_DotNet()
+        {
+            return AntiDebug_C();
+        }
+    }
+}
